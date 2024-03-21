127dc4aa199ac14d503efdf22f452324b7e47673
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-03-21 15:07:01 +0500

    remoteok scraper done

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -2,7 +2,7 @@ from scraper.portals import weworkremotely as wwr
 from scraper.portals import dice
 from scraper.portals import builtin
 from scraper.portals import simplyhired
+from scraper.portals import remoteok
 
-PORTALS = {"weworkremotely": wwr.weworkremotely, "dice": dice.dice, "builtin": builtin.builtin, "remoteok":remoteok.remoteok}
 PORTALS = {"weworkremotely": wwr.weworkremotely, "dice": dice.dice,
-           "builtin": builtin.builtin, "simplyhired": simplyhired.simplyhired}
+           "builtin": builtin.builtin, "simplyhired": simplyhired.simplyhired, "remoteok":remoteok.remoteok}
